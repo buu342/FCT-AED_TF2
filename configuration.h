61dfed3c78d2d1716e7386fdfceea56edb52eee1
Buu342 <buu342@hotmail.com>
2018-05-24 13:40:55 +0100

    Made configuration.h

---
+++ configuration.h
@@ -0,0 +1,15 @@
+#ifndef _CONFIG
+#define _CONFIG
+
+    #define DEBUG_MODE      0
+
+    #define MAX_INPUT       140
+    #define MAX_TRAMPOLINES 500
+    #define MAX_STOCK       3000
+
+    #define NUM_FOOD        3
+    #define FOOD1           'C'
+    #define FOOD2           'S'
+    #define FOOD3           'B'
+
+#endif
