e0b368ffc9de7c209713a2c0b5d718fd3a2dd745
Buu342 <buu342@hotmail.com>
2018-06-02 15:41:15 +0100

    Moved switch statments to reduce repeated code

--- food.h
+++ food.h
@@ -3,7 +3,7 @@
 
     typedef struct _food * food;
 
-    food    food_create(int stock, float price);
+    food    food_create(int stock, float price, char* name);
     void    food_destroy(food f);
     void    food_destroy_all(void* f);
 
@@ -11,5 +11,10 @@
     float   food_get_price(food f);
 
     void    food_set_stock(food f, int stock);
+
+    char*   food_get_name(food f);
+
+    char    food_get_key_from_index(int index);
+    char*   food_get_name_from_index(int index);
 
 #endif
