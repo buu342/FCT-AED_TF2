3586b9c24764b810f5c12f5170f495e3efe88316
Buu342 <buu342@hotmail.com>
2018-06-03 16:32:13 +0100

    Created slot struct

---
+++ slot.h
@@ -0,0 +1,11 @@
+#ifndef _H_QUEUE
+#define _H_QUEUE
+
+    typedef struct _slot * slot;
+
+    slot    slot_create(client c, int num_id);
+    void    slot_destroy(slot s);
+    client  slot_get_client(slot s);
+    int     slot_get_id(slot s);
+
+#endif
