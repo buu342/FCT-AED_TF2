19766d99fa5f2b5fdaa8480ce65c46232767fe9c
Buu342 <buu342@hotmail.com>
2018-06-03 22:08:46 +0100

    Created trampoline destruction functions

--- trampoline.h
+++ trampoline.h
@@ -4,8 +4,15 @@
     typedef struct _trampoline * trampoline;
 
     trampoline  trampoline_create();
-    void        trampoline_set_client(trampoline t, client c, int num_id);
+    void        trampoline_destroy(trampoline t);
+    void        trampoline_destroy_all(void* t);
     bool        trampoline_empty(trampoline t);
+
     client      trampoline_get_client(trampoline t);
+    int         trampoline_get_id(trampoline t);
+
+    void        trampoline_set_client(trampoline t, client c, int num_id);
+
+    void        trampoline_remove_client(trampoline t);
 
 #endif
