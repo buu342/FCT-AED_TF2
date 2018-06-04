ff45a6d34a3b88b7a7b64a8412e51ae6a032dbc7
Buu342 <buu342@hotmail.com>
2018-06-04 17:42:45 +0100

    Properly commented

--- trampoline.c
+++ trampoline.c
@@ -3,11 +3,22 @@
 #include "client.h"
 #include "trampoline.h"
 
+
+/*===================================
+           Global Variables
+===================================*/
+
 struct _trampoline
 {
     client c;
     int num_id;
 };
+
+
+/*===================================
+          trampoline_create
+   Allocate memory for a trampoline
+===================================*/
 
 trampoline trampoline_create()
 {
@@ -24,15 +35,34 @@ trampoline trampoline_create()
     return t;
 }
 
+
+/*===================================
+         trampoline_destroy
+Free the memory used by a trampoline
+===================================*/
+
 void trampoline_destroy(trampoline t)
 {
     free(t);
 }
 
+
+/*===================================
+       trampoline_destroy_all
+     Free the memory used by all
+            trampolines
+===================================*/
+
 void trampoline_destroy_all(void* t)
 {
     trampoline_destroy((trampoline) t);
 }
+
+
+/*===================================
+        trampoline_set_client
+ Set the client using the trampoline
+===================================*/
 
 void trampoline_set_client(trampoline t, client c, int num_id)
 {
@@ -40,21 +70,45 @@ void trampoline_set_client(trampoline t, client c, int num_id)
     t->num_id = num_id;
 }
 
+
+/*===================================
+       trampoline_remove_client
+ Remove a client from the trampoline
+===================================*/
+
 void trampoline_remove_client(trampoline t)
 {
     t->c = NULL;
     t->num_id = 0;
 }
 
+
+/*===================================
+          trampoline_empty
+  Return if the trampoline is empty
+===================================*/
+
 bool trampoline_empty(trampoline t)
 {
     return (t->c == NULL);
 }
 
+
+/*===================================
+        trampoline_get_client
+ Return the client in the trampoline
+===================================*/
+
 client trampoline_get_client(trampoline t)
 {
     return t->c;
 }
+
+
+/*===================================
+          trampoline_get_id
+   Return the ID in the trampoline
+===================================*/
 
 int trampoline_get_id(trampoline t)
 {
